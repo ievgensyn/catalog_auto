273c7b573d20c10df3b277846e4cdf0f213c7058
Ievgen Synchyshyn <synchyshyn.i@gmail.com>
2020-08-18 12:11:07 +0300

    update (fields)

--- catalog_auto/models/product_template.py
+++ catalog_auto/models/product_template.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from odoo import fields, models, _
+from odoo import fields, models, api, _
 
 
 class ProductTemplate(models.Model):
@@ -12,23 +12,32 @@ class ProductTemplate(models.Model):
     ]
 
     catalog_auto_year = fields.Char(string='Year')
-    catalog_auto_car_color = fields.Many2one(
+    catalog_auto_car_color_id = fields.Many2one(
         comodel_name='catalog.auto.color', string='Color')
     catalog_auto_engine_code = fields.Char(size=10, string='Engine code')
-    catalog_auto_transmission = fields.Many2one(
+    catalog_auto_transmission_id = fields.Many2one(
         comodel_name='catalog.auto.transmission', string='Transmission')
-    catalog_auto_parts = fields.Many2one(
+    catalog_auto_parts_id = fields.Many2one(
         comodel_name='catalog.auto.parts', string='Parts')
-    catalog_auto_engine_volume = fields.Many2one(
+    catalog_auto_engine_volume_id = fields.Many2one(
         comodel_name='catalog.auto.engine.volume', string='Engine volume')
-    catalog_auto_model = fields.Many2one(
+    catalog_auto_model_id = fields.Many2one(
         comodel_name='catalog.auto.model', string='Model')
-    catalog_auto_vin = fields.Many2one(
-        comodel_name='catalog.auto.vin', string='VIN')
-    catalog_auto_fuel_type = fields.Many2one(
+    catalog_auto_vin_id = fields.Many2one(
+        comodel_name='catalog.auto.vin', string='VIN',
+        domain=[])
+    catalog_auto_fuel_type_id = fields.Many2one(
         comodel_name='catalog.auto.fuel.type', string='Fuel type')
-    catalog_auto_power = fields.Many2one(
+    catalog_auto_power_id = fields.Many2one(
         comodel_name='catalog.auto.power', string='Power')
     catalog_auto_last_odometer_value = fields.Integer(
         readonly=False, track_visibility='onchange',
         string='Odometer value')
+
+    @api.onchange('catalog_auto_model_id')
+    def onchange_catalog_auto_model_id(self):
+        if self.catalog_auto_model_id and self.catalog_auto_model_id.vin_ids:
+            ids = self.catalog_auto_model_id.vin_ids.ids
+            domain = [('id', 'in', ids)]
+            return {'domain': {'catalog_auto_vin_id': domain}}
+
