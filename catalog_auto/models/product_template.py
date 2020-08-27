2089f2c98ec1dd492ab13ba6ae6f927b4a53e1b8
ievgen <synchyshyn.i@web.de>
2020-08-27 18:20:48 +0300

    [UPDATE] m2o, o2m fields add suffix _id, _ids

--- catalog_auto/models/product_template.py
+++ catalog_auto/models/product_template.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from odoo import fields, models, _
 
 
@@ -12,22 +11,22 @@ class ProductTemplate(models.Model):
     ]
 
     catalog_auto_year = fields.Char(string='Year')
-    catalog_auto_car_color = fields.Many2one(
+    catalog_auto_car_color_id = fields.Many2one(
         comodel_name='catalog.auto.color', string='Color')
     catalog_auto_engine_code = fields.Char(size=10, string='Engine code')
-    catalog_auto_transmission = fields.Many2one(
+    catalog_auto_transmission_id = fields.Many2one(
         comodel_name='catalog.auto.transmission', string='Transmission')
-    catalog_auto_parts = fields.Many2one(
+    catalog_auto_part_id = fields.Many2one(
         comodel_name='catalog.auto.parts', string='Parts')
-    catalog_auto_engine_volume = fields.Many2one(
+    catalog_auto_engine_volume_id = fields.Many2one(
         comodel_name='catalog.auto.engine.volume', string='Engine volume')
-    catalog_auto_model = fields.Many2one(
+    catalog_auto_model_id = fields.Many2one(
         comodel_name='catalog.auto.model', string='Model')
-    catalog_auto_vin = fields.Many2one(
+    catalog_auto_vin_id = fields.Many2one(
         comodel_name='catalog.auto.vin', string='VIN')
-    catalog_auto_fuel_type = fields.Many2one(
+    catalog_auto_fuel_type_id = fields.Many2one(
         comodel_name='catalog.auto.fuel.type', string='Fuel type')
-    catalog_auto_power = fields.Many2one(
+    catalog_auto_power_id = fields.Many2one(
         comodel_name='catalog.auto.power', string='Power')
     catalog_auto_last_odometer_value = fields.Integer(
         readonly=False, track_visibility='onchange',
